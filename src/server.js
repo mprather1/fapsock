a19103a6fdb76317b086dcc06bfa34a8043f9316
Michael Prather <mprather1@gmail.com>
2017-04-01 13:49:52 -0500

    updated server

--- src/server.js
+++ src/server.js
@@ -1,22 +1,38 @@
 import net from 'net'
 
 export default function getSocketServer (options) {
-  const {logger} = options
+  const {port, logger, environment} = options
   
-  const server = net.createServer(function (socket) {
-    socket.name = socket.remoteAddress + ':' + socket.remotePort
-    
-    socket.on('end', () => {
-      logger.info('disconnected')
-    })
-    
-    socket.on('connection', () => {
-      logger.info('connected', socket)  
-    })
-  })
+  const server = net.createServer()
   
-  server.listen(8001, () => {
-    console.log("listening")
+  server.on('connection', (data) => {
+    handleConnection(data, logger)
+  })  
+  server.listen(port, function () {
+    logger.info('Environment:', environment)
+    logger.info('Listening to port:', port + '...')
   })
   return server
+}
+
+function handleConnection (socket, logger) {
+  const remoteAddress = socket.remoteAddress + ':' + socket.remotePort
+  logger.info('new client connection from %s', remoteAddress)
+  
+  socket.on('data', onSocketData)
+  socket.on('close', onSocketClose)
+  socket.on('errror', onSocketError)
+  
+  function onSocketData (d) {
+    logger.info('connection data from', remoteAddress, d)
+    socket.write(d)
+  }
+  
+  function onSocketClose () {
+    logger.info('connection from', remoteAddress, 'closed')
+  }
+  
+  function onSocketError (err) {
+    logger.info('connection', remoteAddress, 'error:', err.message)
+  }
 }
