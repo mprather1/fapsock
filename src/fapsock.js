1b5096b69315f1f483403b5a87ecabd9229a2307
Michael Prather <mprather1@gmail.com>
2017-04-01 15:37:28 -0500

    json

--- src/fapsock.js
+++ src/fapsock.js
@@ -1,14 +1,16 @@
 import net from 'net'
+import JSONDuplexStream from 'json-duplex-stream'
+import Gateway from './gateway'
 
 export default function getSocketServer (options) {
   const {port, logger, environment} = options
   
   const server = net.createServer()
   
-  server.on('connection', (data) => {
-    handleConnection(data, logger)
+  server.on('connection', (socket) => {
+    handleConnection(socket, logger)
   })  
-  server.listen(port, function () {
+  server.listen(port, () => {
     logger.info('Environment:', environment)
     logger.info('Listening to port:', port + '...')
   })
@@ -19,15 +21,28 @@ function handleConnection (socket, logger) {
   const remoteAddress = socket.remoteAddress + ':' + socket.remotePort
   logger.info('new client connection from %s', remoteAddress)
   
-  socket.setEncoding('utf8')
+  const s = JSONDuplexStream()
+  const gateway = Gateway()
   
+  socket.
+    pipe(s.in).
+    pipe(gateway).
+    pipe(s.out).
+    pipe(socket)
+
+  s.in.on('error', onProtocolError)
+  s.out.on('error', onProtocolError)
   socket.on('data', onSocketData)
   socket.on('close', onSocketClose)
   socket.on('errror', onSocketError)
   
+  function onProtocolError (err) {
+    socket.end('protocol error: ' + err.message)
+  }
+  
   function onSocketData (d) {
-    logger.info('connection data from', remoteAddress, d)
-    socket.write(d.toUpperCase())
+    logger.info('connection data from', remoteAddress)
+    socket.write(d)
   }
   
   function onSocketClose () {
@@ -38,4 +53,3 @@ function handleConnection (socket, logger) {
     logger.error('connection', remoteAddress, 'error:', err.message)
   }
 }
-
