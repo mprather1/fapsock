1315389a0fa60e725634b35a9e04512bcd816f33
Michael Prather <mprather1@gmail.com>
2017-04-07 00:40:37 +0000

    1.0.0 -> now a client instead of a server

--- src/fapsock.js
+++ src/fapsock.js
@@ -1,55 +1,44 @@
 import net from 'net'
-import JSONDuplexStream from 'json-duplex-stream'
-import Gateway from './gateway'
+import DuplexEmitter from 'duplex-emitter'
+import winston from 'winston-color'
+import chalk from 'chalk'
+import pkg from '../package.json'
 
-export default function getSocketServer (options) {
-  const {port, logger, environment} = options
+export default function getSocketClient (options) {
   
-  const server = net.createServer()
+  const {socketPort, hostname, packageName, logger} = options
   
-  server.on('connection', (socket) => {
-    handleConnection(socket, logger)
-  })  
-  server.listen(port, () => {
-    logger.info('Environment:', environment)
-    logger.info('Listening to port:', port + '...')
+  const s = new net.Socket()
+  
+  const socket = s.connect(socketPort, hostname)
+  
+  const emitter = DuplexEmitter(socket)
+
+  socket.on('connect', () => {
+    emitter.emit('connected', packageName)
   })
-  return server
+  
+  emitter.on('connected', (data) => {
+    logger.info(`connected to ${chalk.bgBlack.green(data)}...`)
+    socket.on('close', () => {
+      handleClose(data, data)
+    })    
+  })
+  
+  function handleClose (name) {
+    logger.info(`connection to ${chalk.bgBlack.green(name)} closed...`)
+  }
+  
+  options.socket = socket
+  options.emitter = emitter
+  
+  return options
 }
 
-function handleConnection (socket, logger) {
-  const remoteAddress = socket.remoteAddress + ':' + socket.remotePort
-  logger.info('new client connection from %s', remoteAddress)
-  
-  const s = JSONDuplexStream()
-  const gateway = Gateway()
-  
-  socket.
-    pipe(s.in).
-    pipe(gateway).
-    pipe(s.out).
-    pipe(socket)
-
-  s.in.on('error', onProtocolError)
-  s.out.on('error', onProtocolError)
-  socket.on('data', onSocketData)
-  socket.on('close', onSocketClose)
-  socket.on('errror', onSocketError)
-  
-  function onProtocolError (err) {
-    socket.end('protocol error: ' + err.message)
-  }
-  
-  function onSocketData (d) {
-    logger.info('connection data from', remoteAddress)
-    socket.write(d)
-  }
-  
-  function onSocketClose () {
-    logger.warn('connection from', remoteAddress, 'closed')
-  }
-  
-  function onSocketError (err) {
-    logger.error('connection', remoteAddress, 'error:', err.message)
-  }
+const options = {
+  socketPort: process.env.SOCKET_PORT,
+  environment: process.env.NODE_ENV || 'development',
+  logger: winston,
+  hostname: process.env.HOSTNAME,
+  packageName: pkg.name,
 }
