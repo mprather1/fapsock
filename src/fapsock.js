59e100ece8ca6f91904077ca86255e99f9b1d684
Michael Prather <mprather1@gmail.com>
2017-04-09 03:54:48 +0000

    1.2.0 -> reconnect-net

--- src/fapsock.js
+++ src/fapsock.js
@@ -1,50 +1,69 @@
 import net from 'net'
+import reconnect from 'reconnect-net'
 import DuplexEmitter from 'duplex-emitter'
 import chalk from 'chalk'
 
 export default class Fapsock {
   constructor (options) {
-    const {socketPort, hostname} = options
-    
-    const s = new net.Socket()
-    const socket = s.connect(socketPort, hostname)
-    const emitter = DuplexEmitter(socket)
-    
-    configClient(options, socket, emitter)
-    
-    this.socket = socket
-    this.emitter = emitter
+  const {socketPort, hostname} = options
+  
+  const socket = reconnect().connect(socketPort, hostname)
+  const emitter = DuplexEmitter(socket._connection)
+  
+  configClient(options, socket, emitter)
+  
+  this.socket = socket
+  this.emitter = emitter
   }
 }
 
 function configClient (options, socket, emitter) {
   const {packageName, logger} = options
   
-  socket.on('connect', () => {
-    emitter.emit('connected', packageName)
+  let serverName
+  
+  socket.once('connect', () => {
+    handleConnection(socket, logger)
+  })
+
+  emitter.once('connected', (name) => {
+    logger.info(`connected to ${chalk.bgBlack.green(name)}...`)
+
+    serverName = name
+    
+    socket.once('disconnect', () => {
+      handleClose(socket, name)
+    })
   })
   
   socket.on('error', (err) => {
     handleError(err, logger)
   })
   
-  emitter.on('connected', (data) => {
-    logger.info(`connected to ${chalk.bgBlack.green(data)}...`)
-    socket.on('close', () => {
-      handleClose(data, data)
-    })    
-  })
-  
   emitter.on('error', (err) => {
     handleEmitterError(err, logger)
   })
   
-  function handleEmitterError (err, logger) {
-    logger.error('error:', err.message)
+  function handleConnection (socket, logger) {
+    socket.on('reconnect', () => {
+      logger.error(`connection to ${chalk.bgBlack.green(serverName)} lost; attempting to reconnect...`)
+      handleReconnection(socket, logger)
+    })
+    emitter.emit('connected', packageName)    
   }
   
-  function handleClose (name) {
-    logger.info(`connection to ${chalk.bgBlack.green(name)} closed...`)
+  function handleReconnection (socket, logger) {
+    socket.once('connect', () => {
+      logger.info(`successfully reconnected to ${chalk.bgBlack.green(serverName)}...`)
+    })    
+  }
+
+  function handleClose (socket, name) {
+    logger.warn(`disconnected from ${chalk.bgBlack.green(name)}...`)
+  }
+  
+  function handleEmitterError (err, logger) {
+    logger.error('error:', err.message)
   }
 }
 
